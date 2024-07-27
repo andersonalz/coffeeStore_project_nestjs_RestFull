2cda786b6d31ade781585ae4dae6323272acc68e
Ehsan_Alizadegan <ehsan.alizadegan55@gmail.com>
2024-07-27 16:08:11 +0330

    add parsInt pipe

--- src/common/pipes/pars-int/pars-int.pipe.ts
+++ src/common/pipes/pars-int/pars-int.pipe.ts
@@ -3,10 +3,11 @@ import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from
 @Injectable()
 export class ParsIntPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
+    console.log("🚀 ~ ParsIntPipe ~ transform ~ value:", value)
     const val = Number.parseInt(value, 10);
     if(isNaN(val)){
       throw new BadRequestException('validation failed input data is not a number');
     }
-    return value;
+    return val;
   }
 }
